beacbf7a7675a37105320201915d33b5803f7e64
unknown <dhanupriasokan@gmail.com>
2023-12-05 10:21:26 +0530

    return sad

--- src/main/java/Mood_Analayser.java
+++ src/main/java/Mood_Analayser.java
@@ -1,14 +1,14 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Mood_Analayser {
     String analyseMood(String message){
 
-        if(message.equals("Happy")){
-            return "Happy";
-        }
-        if(message.equals("Sad")){
-            return "Sad";
-        }
+            if(message.toLowerCase().contains("sad")){
+                return "SAD";
+            }
+
+
         return "unknown";
 
     }
@@ -16,7 +16,9 @@ public class Mood_Analayser {
 
         Mood_Analayser mood_analayser=new Mood_Analayser();
         Scanner sc=new Scanner(System.in);
-        String mood =mood_analayser.analyseMood(sc.nextLine());
+        String sentence=sc.nextLine();
+
+        String mood =mood_analayser.analyseMood(sentence);
 
         System.out.println("The mood is:"+mood);
 
