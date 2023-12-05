3d98960521b8125b47ffee37571e5df91f27c3d6
unknown <dhanupriasokan@gmail.com>
2023-12-05 10:39:18 +0530

    refactor

--- src/main/java/Mood_Analayser.java
+++ src/main/java/Mood_Analayser.java
@@ -2,7 +2,28 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class Mood_Analayser {
-    String analyseMood(String message){
+    String message;
+    //default constructor
+    Mood_Analayser(){
+        this.message="";
+
+    }
+
+    //constructor with parameter
+    Mood_Analayser(String message){
+        this.message=message;
+
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    String analyseMood(){
 
             if(message.toLowerCase().contains("sad")){
                 return "SAD";
@@ -16,14 +37,23 @@ public class Mood_Analayser {
 
     }
     public static void main(String[] args){
-
-        Mood_Analayser mood_analayser=new Mood_Analayser();
         Scanner sc=new Scanner(System.in);
+
         String sentence=sc.nextLine();
+        //default constructor
+        Mood_Analayser mood_analayser1=new Mood_Analayser();
+        mood_analayser1.setMessage(sentence);
+        String result_mood=mood_analayser1.analyseMood();
+        System.out.println("The mood is :"+result_mood);
 
-        String mood =mood_analayser.analyseMood(sentence);
 
-        System.out.println("The mood is:"+mood);
+        //Constructor with parameters
+        Mood_Analayser mood_analayser=new Mood_Analayser(sentence);
+
+
+        String mood =mood_analayser.analyseMood();
+
+        System.out.println("The mood is :"+mood);
 
 
 
